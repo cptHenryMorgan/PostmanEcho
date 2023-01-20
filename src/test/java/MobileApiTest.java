bb81c9e3430437244f23712df60c3935775c8c01
Artem Egorov <Artem-Egorov13@mail.ru>
2023-01-20 14:19:46 +0300

    add post Cyrillic

--- src/test/java/MobileApiTest.java
+++ src/test/java/MobileApiTest.java
@@ -14,7 +14,7 @@ public class MobileApiTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello, World!"))
+                .body("data", equalTo("Привет,Мир!"))
         ;
     }
 }
